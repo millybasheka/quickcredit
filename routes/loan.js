24d3b52b647b4fc1d2304899deebbf32a20e20eb
kliques <elemanhillary@gmail.com>
2019-05-02 02:05:48 -0400

    Add files via upload

---
+++ routes/loan.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const loanController = require('../controllers/loan');
+const { checkToken } = require('../middleware/auth');
+
+const router = express.Router();
+/**
+ *
+ * apply for loan router
+ *
+ */
+
+router.post('/loans', checkToken, loanController.loanApply);
+
+/**
+  *
+  *
+  * apply for loan repayment
+  *
+  */
+router.post('/loans/:id/repayments', checkToken, loanController.repay);
+
+/**
+  *
+  *
+  * apply for loan repayment(admin)
+  *
+  */
+router.post('/loans/repayments', loanController.repay);
+
+
+/**
+ *
+ *
+ * view loan repayment history
+ *
+ */
+
+router.get('/loans/:id/repayments', checkToken, loanController.userRepayHist);
+
+module.exports = router;
