dfebaf6ae2ca8a41d12c7fb1ac0a9d27c5bb11f1
kliques <elemanhillary@gmail.com>
2019-05-02 05:10:10 -0400

    Update repay.js

--- client/js/repay.js
+++ client/js/repay.js
@@ -1,14 +1,23 @@
-const submit_app = document.querySelector(".submit_app");
-submit_app.onclick = (e) => {
-	postFormData(`https://qwikcredit.herokuapp.com/api/v1/loans/${localStorage.getItem('loan_id')}/repayments`)
+const amount = document.querySelector('.amount input[name = paidAmount]');
+const repayBtn = document.querySelector('.submit_app');
+
+async function repay(url, amount) {
+	const response = await fetch(url, {
+		method: 'POST',
+		body: new URLSearchParams({ amount: amount }),
+		headers: new Headers({ 'Content-Type': 'application/x-www-form-urlencoded', 
+				     'Authorization': localStorage.getItem('token'),
+				     })
+	})
+	return await response.json();
+};
+
+repayBtn.onclick = function(_e) {
+	repay(`https://qwikcredit.herokuapp.com/api/v1/loans/${localStorage.getItem('loan_id')}/repayments`, amount.value)
 	.then(data => {
 		console.log(data);
 		if (data.Success) {
-			if(data.data.isAdmin) {
-				window.location.href = 'https://elemanhillary.github.io/QuickCredit/admin/'
-			} else {
-				window.location.href = 'https://elemanhillary.github.io/QuickCredit/client/';
-			}
+			console.log('yes')
 		} else if (data.status === 422){
 			errors.textContent = data.message;
 			errors.style.display = 'block'
@@ -23,19 +32,8 @@ submit_app.onclick = (e) => {
 			}, 1500)
 		}
 	})
-	.catch(error => console.error(error))
-
-	async function postFormData(url, data) {
-		const formData = new FormData(document.querySelector('.repayment_form form'))
-		const response = await fetch(url, {
-			method: 'POST',
-			body: new URLSearchParams(formData),
-			headers: new Headers({
-				'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-				'Authorization': localStorage.getItem('token'),
-			})
-		});
-		return await response.json();
-	}
+	.catch(error => {
+		console.error(error);
+	})
+	
 }
-
