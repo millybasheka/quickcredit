7abf74e78ff782467d8d12ab0310f91b62d7dc00
kliques <elemanhillary@gmail.com>
2019-05-02 17:46:59 -0400

    Update postloan.js

--- admin/js/postloan.js
+++ admin/js/postloan.js
@@ -2,17 +2,17 @@ const email = document.querySelector('.amount input[name = email]');
 const amount = document.querySelector('.amount input[name = paidAmount]');
 const repayBtn = document.querySelector('.submit_app');
 
-async function repay(url, email, amount) {
+function repay(url, email, amount) {
 	const response = await fetch(url, {
 		method: 'POST',
 		body: new URLSearchParams({ amount: amount, email: email }),
 		headers: new Headers({ 'Content-Type': 'application/x-www-form-urlencoded'})
 	})
-	return await response.json();
+	return response.json();
 };
 
 repayBtn.onclick = function(_e) {
-	repay('http://localhost:3000/api/v1/loans/repayments', email.value, amount.value)
+	repay('https://qwikcredit.herokuapp.com/api/v1/loans/repayments', email.value, amount.value)
 	.then(data => {
 		console.log(data);
 	})
