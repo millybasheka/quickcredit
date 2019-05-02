defc2bf74e291e69d0add380438fdbbeeaf9cb89
kliques <elemanhillary@gmail.com>
2019-05-02 05:27:48 -0400

    Update repay.js

--- client/js/repay.js
+++ client/js/repay.js
@@ -1,25 +1,43 @@
-const amount = document.querySelector('.amount input[name = paidAmount]');
-const repayBtn = document.querySelector('.submit_app');
-
-async function repay(url, amount) {
-	const response = await fetch(url, {
-		method: 'POST',
-		body: new URLSearchParams({ amount: amount }),
-		headers: new Headers({ 'Content-Type': 'application/x-www-form-urlencoded', 
-				     'Authorization': localStorage.getItem('token'),
-				     })
-	})
-	return await response.json();
-};
-
-repayBtn.onclick = function(_e) {
-	repay(`https://qwikcredit.herokuapp.com/api/v1/loans/${localStorage.getItem('loan_id')}/repayments`, amount.value)
+let submit = document.querySelector('.submit_app');
+submit.onclick = (e) => {
+	postFormData(`https://qwikcredit.herokuapp.com/api/v1/loans/${localStorage.getItem(loan_id)`)
 	.then(data => {
-		console.log(data);
-		console.log(`https://qwikcredit.herokuapp.com/api/v1/loans/${localStorage.getItem('loan_id')}/repayments`)
+console.log(data)
+		if (data.status === 201) {
+			console.log(data)
+		} else if (data.status === 422){
+			errors.textContent = data.message;
+			errors.style.display = 'block'
+			setTimeout(function() {
+				errors.style.display = 'none'
+			}, 1500)			
+		} else if (data.status === 403) {
+			errors.textContent = data.error;
+			errors.style.display = 'block'
+			setTimeout(function() {
+				errors.style.display = 'none'
+			}, 1500)
+		} else {
+			errors.textContent = data.error;
+			errors.style.display = 'block'
+			setTimeout(function() {
+				errors.style.display = 'none'
+			}, 1500)
+		}
 	})
-	.catch(error => {
-		console.error(error);
-	})
-	
+	.catch(error => console.error(error))
+
+	async function postFormData(url) {
+		const formData = new FormData(document.querySelector('.repayment_form form'))
+		const response = await fetch(url, {
+			method: 'POST',
+			body: new URLSearchParams(formData),
+			headers: new Headers({
+				'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+				'Authorization': localStorage.getItem('token')
+			})
+		});
+		return await response.json();
+	}
+
 }
