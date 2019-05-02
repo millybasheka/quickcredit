1f8b409ec0957e82dbc65af8cc7b4960ba652ba6
kliques <elemanhillary@gmail.com>
2019-05-02 03:42:06 -0400

    Update repay.js

--- client/js/repay.js
+++ client/js/repay.js
@@ -1,3 +1,12 @@
+async function postFormData(url, data) {
+	const formData = new FormData(document.querySelector('.sign_up form'))
+	const response = await fetch(url, {
+		method: 'POST',
+		body: new URLSearchParams(formData),
+		headers: new Headers({ 'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8' })
+	});
+	return await response.json();
+}
 function getRepays(url) {
 	return fetch(url, {
 	method: 'GET',
