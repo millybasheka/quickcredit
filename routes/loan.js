525e511704bea0e18c2cd817a06d81027c66afd9
kliques <elemanhillary@gmail.com>
2019-05-06 14:13:16 -0400

    Add files via upload

--- routes/loan.js
+++ routes/loan.js
@@ -8,14 +8,9 @@ const router = express.Router();
  * apply for loan router
  *
  */
-
+router.get('/compiled', loanController.compiled);
 router.post('/loans', checkToken, loanController.loanApply);
-/**
- *
- * get compiled history
- *
- */
-router.get('/compiled', checkToken, loanController.compiled);
+
 /**
   *
   *
