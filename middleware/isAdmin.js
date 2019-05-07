f119f281c8cb8f3fa8db50291926b5b08c1d891d
kliques <elemanhillary@gmail.com>
2019-05-07 00:17:46 -0400

    Add files via upload

--- middleware/isAdmin.js
+++ middleware/isAdmin.js
@@ -1,19 +1,19 @@
 const { newUser } = require('../helper/util');
 
 const checkAdmin = (req, res, next) => {
-	if (req.decoded) {
-		const email = req.decoded.user.trim();
-		const isAdmin = newUser.checkEmail(email).node.isAdmin;
-		if (isAdmin) {
-			next();
-		} else {
-			res.status(401).json({ status: 401, error: 'Admin only please' });
-		}
-	} else {
-		res.status(499).json({ status: 422, error: 'unprocessible entity' });
-	}
+  if (req.decoded) {
+    const email = req.decoded.user.trim();
+    const { isAdmin } = newUser.checkEmail(email).node;
+    if (isAdmin) {
+      next();
+    } else {
+      res.status(401).json({ status: 401, error: 'Admin only please' });
+    }
+  } else {
+    res.status(499).json({ status: 422, error: 'unprocessible entity' });
+  }
 };
 
 module.exports = {
-	checkAdmin,
+  checkAdmin,
 };
