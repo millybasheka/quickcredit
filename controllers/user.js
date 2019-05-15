e1f911f5ef0cd676d8f18a3396197d4f5d59b277
kliques <elemanhillary@gmail.com>
2019-05-15 05:59:53 -0700

    Update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -89,7 +89,7 @@ const signin = (req, res) => {
   }
   return res.status(401).json({
     status: 401,
-    message: 'WWrong Password or Email',
+    message: 'Wrong Password or Email',
   });
 };
 
